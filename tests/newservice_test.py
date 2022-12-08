75e76e0fb09dd01f9b4f7c8f9778a8742a99f5fd
Israel Mateos Aparicio <71361287+israelMateos@users.noreply.github.com>
2022-12-09 00:52:42 +0100

    Implementado test_auth_proxy

--- tests/newservice_test.py
+++ tests/newservice_test.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import patch
+from iceflix.main import Main
 
+SERVICE_ID = "test_id"
+RESPONSE_TIME = 30
 
 class NewServiceTesting(unittest.TestCase):
-    def test_auth_proxy(self):
-        pass
+    @patch('IceFlix.AuthenticatorPrx')
+    def test_auth_proxy(self, mock_proxy):
+        main = Main()
+        self.assertFalse(main.authenticator_services)
+        main.newService(mock_proxy, SERVICE_ID, None)
+        self.assertEqual(main.authenticator_services[SERVICE_ID],
+            [mock_proxy.checkedCast(), RESPONSE_TIME])
 
     def test_catalog_proxy(self):
         pass
