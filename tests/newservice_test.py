4ea1780d639cf34694cf70068a4a67fc6a79fa90
Israel Mateos Aparicio <71361287+israelMateos@users.noreply.github.com>
2022-12-09 00:59:55 +0100

    Imitación de funciones checkedCast()

--- tests/newservice_test.py
+++ tests/newservice_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+"""Module containing tests for newService() method from Main service."""
 
 import unittest
 from unittest.mock import patch
@@ -7,9 +7,34 @@ from iceflix.main import Main
 SERVICE_ID = "test_id"
 RESPONSE_TIME = 30
 
+def mock_auth_checked_cast(proxy):
+    """Checks if the mock proxy passed as an argument is mocking an
+    Authenticator proxy."""
+    if proxy.name == 'AuthenticatorPrx':
+        return proxy
+    return None
+
+def mock_catalog_checked_cast(proxy):
+    """Checks if the mock proxy passed as an argument is mocking an
+    MediaCatalog proxy."""
+    if proxy.name == 'MediaCatalogPrx':
+        return proxy
+    return None
+
+def mock_file_checked_cast(proxy):
+    """Checks if the mock proxy passed as an argument is mocking an
+    FileService proxy."""
+    if proxy.name == 'FileServicePrx':
+        return proxy
+    return None
+
+
 class NewServiceTesting(unittest.TestCase):
+    """Tests newService() method from Main service."""
+
     @patch('IceFlix.AuthenticatorPrx')
     def test_auth_proxy(self, mock_proxy):
+        """Tests newService() method with an Authenticator proxy as input."""
         main = Main()
         self.assertFalse(main.authenticator_services)
         main.newService(mock_proxy, SERVICE_ID, None)
