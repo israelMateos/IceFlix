2dacd336785ea867f8d3c3135307304105328f0d
Israel Mateos Aparicio <71361287+israelMateos@users.noreply.github.com>
2022-12-09 00:49:10 +0100

    Definidos métodos para testear newService()

---
+++ tests/newservice_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class NewServiceTesting(unittest.TestCase):
+    def test_auth_proxy(self):
+        pass
+
+    def test_catalog_proxy(self):
+        pass
+
+    def test_file_proxy(self):
+        pass
+
+    def test_invalid_proxy(self):
+        pass
