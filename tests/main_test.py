9a86cbf37907e9dc478849116af840f29437e730
Israel Mateos Aparicio <71361287+israelMateos@users.noreply.github.com>
2023-01-02 22:23:14 +0100

    Actualizadas pruebas a comunicación indirecta

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,8 +1,7 @@
-"""Module containing tests for getAuthenticator(), getCatalog() and
-getFileService() methods from Main service."""
+"""Module containing tests for Main service class."""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from iceflix.main import Main
 try:
     import IceFlix
@@ -125,3 +124,32 @@ class GetProxyTesting(unittest.TestCase):
         with patch('IceFlix.FileServicePrx.ice_ping') as mock_ice_ping:
             mock_ice_ping.return_value = None
             self.assertEqual(self.main.getFileService(), mock_proxy)
+
+    def test_unexpired_proxys(self):
+        """Tests check_timeouts() method with unexpired proxys saved in
+        cache."""
+        obj = MagicMock()
+        obj.name = 'object'
+        self.main.authenticator_services[SERVICE_ID] = [obj, RESPONSE_TIME]
+        self.main.catalog_services[SERVICE_ID] = [obj, RESPONSE_TIME]
+        self.main.file_services[SERVICE_ID] = [obj, RESPONSE_TIME]
+        self.main.check_timeouts()
+        self.assertEqual(self.main.authenticator_services[SERVICE_ID][1],
+            RESPONSE_TIME - 1)
+        self.assertEqual(self.main.catalog_services[SERVICE_ID][1],
+            RESPONSE_TIME - 1)
+        self.assertEqual(self.main.file_services[SERVICE_ID][1],
+            RESPONSE_TIME - 1)
+
+    def test_to_be_expired_proxys(self):
+        """Tests check_timeouts() method with proxys which will be expired in
+        one second saved in cache."""
+        obj = MagicMock()
+        obj.name = 'object'
+        self.main.authenticator_services[SERVICE_ID] = [obj, 1]
+        self.main.catalog_services[SERVICE_ID] = [obj, 1]
+        self.main.file_services[SERVICE_ID] = [obj, 1]
+        self.main.check_timeouts()
+        self.assertFalse(self.main.authenticator_services)
+        self.assertFalse(self.main.catalog_services)
+        self.assertFalse(self.main.file_services)
